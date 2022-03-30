d67bdbbd6572e77c76bb0e14914fff80ac5812ac
bacvu91 <68065092+bacvu91@users.noreply.github.com>
2022-03-30 23:25:19 +0700

    Add files via upload

---
+++ bacvu91.github.io/myScript.js
@@ -0,0 +1,11 @@
+
+window.onscroll = function() {myFunction()};
+window.onresize = doALoadOfStuff;
+
+var width = screen.width;
+
+var sticky = sidebar.offsetTop;
+
+function doALoadOfStuff() {
+    r
+}
